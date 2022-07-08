b7825003f4acf127e875a7efbd4e8a77dfd16433
ClariS <1457715339@qq.com>
2022-07-08 23:25:16 +0800

    引入 vue router 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0);
-    const onClick = () => {
-      refCount.value += 1;
-    };
-    return () => (
-      <>
-        <div>{refCount.value}</div>
-        <div>
-          <button onClick={onClick}>+1</button>
-        </div>
-      </>
-    );
-  },
-});
+    return () => <>
+      <header>导航
+        <ul>
+          <li>
+            <router-link to="/">Foo</router-link>
+          </li>
+          <li>
+            <router-link to="/about">Bar</router-link>
+          </li>
+        </ul> 
+      </header>
+      <div>
+        <RouterView />
+      </div>
+      <footer>页脚</footer>
+    </>
+
+  }
+})
