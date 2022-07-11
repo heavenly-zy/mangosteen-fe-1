c18af03248377d70c2c7b666a806276658dddce4
ClariS <1457715339@qq.com>
2022-07-11 14:28:43 +0800

    重构 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { Bar } from './views/Bar'
-import { Foo } from './views/Foo'
-import {createRouter, createWebHashHistory} from 'vue-router'
+import {createRouter} from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
-const routes = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes, 
-})
+const router = createRouter({ history,routes })
 
 const app = createApp(App)
 app.use(router)
