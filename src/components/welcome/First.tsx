ab1c01dfd55ea47d9c1536ca3b9091e1bf05739e
ClariS <1457715339@qq.com>
2022-07-12 20:11:40 +0800

    feature: 第一个欢迎界面

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,8 +1,23 @@
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import s from './First.module.scss';
+import pig from '../../assets/icons/pig.svg'
 export const First = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div>First</div>
-    )
-  }
-})
+  setup: (props, context) => {
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.pig} src={pig} alt="" />
+          <h2>会挣钱<br />还要会省钱</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/welcome/2">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </div>
+      </div>
+    );
+  },
+});
