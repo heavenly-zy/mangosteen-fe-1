57f19f47b18d2cf0d404c69d9d3d6d342388acaf
ClariS <1457715339@qq.com>
2022-07-14 20:25:59 +0800

    修改默认路由

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import {createRouter} from 'vue-router'
+import { createRouter } from 'vue-router'
 import { routes } from './config/routes'
 import { history } from './shared/history'
 
-const router = createRouter({ history,routes })
+const router = createRouter({ history, routes })
 
 const app = createApp(App)
 app.use(router)
