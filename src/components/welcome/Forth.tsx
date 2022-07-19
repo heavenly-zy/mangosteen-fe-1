69658c755f07d2dca75f46ece644a7509a8fbb14
ClariS <1457715339@qq.com>
2022-07-19 20:34:56 +0800

    feat: 四个欢迎页面

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,6 +1,23 @@
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import s from './First.module.scss';
+import cloud from '../../assets/icons/cloud.svg'
 export const Forth = defineComponent({
   setup: (props, context) => {
-    return () => <div>Forth</div>;
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.pig} src={cloud} alt="" />
+          <h2>云备份<br/>再也不怕数据丢失</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/start">开启应用</RouterLink>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+        </div>
+      </div>
+    );
   },
 });
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,6 +1,23 @@
 import { defineComponent } from 'vue';
+import { RouterLink } from 'vue-router';
+import s from './First.module.scss';
+import chart from '../../assets/icons/chart.svg'
 export const Third = defineComponent({
   setup: (props, context) => {
-    return () => <div>Third</div>;
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.pig} src={chart} alt="" />
+          <h2>数据可视化<br/>收支一目了然</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink to="/welcome/4">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </div>
+      </div>
+    );
   },
 });
