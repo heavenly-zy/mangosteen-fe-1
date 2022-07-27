d5fb7d2a51b1909c90b84bca8fbefbbbca4b1d27
ClariS <1457715339@qq.com>
2022-07-27 16:47:56 +0800

    feat: 添加欢迎界面的路由过渡动画

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,7 +1,8 @@
-import { defineComponent } from 'vue';
-import { RouterView } from 'vue-router';
+import { defineComponent, Transition, VNode } from 'vue';
+import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
 import s from './Welcome.module.scss'
 import logo from '../assets/icons/mangosteen.svg'
+
 export const Welcome = defineComponent({
   setup: (props, context) => {
     return () => <div class={s.wrapper}>
@@ -10,7 +11,18 @@ export const Welcome = defineComponent({
         <h1>山竹记账</h1>
       </header>
       <main class={s.main}>
-        <RouterView name='main' />
+        <RouterView name='main'>
+          {({ Component: Content, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
+            <Transition
+              enterFromClass={s.slide_fade_enter_from}
+              enterActiveClass={s.slide_fade_enter_active}
+              leaveToClass={s.slide_fade_leave_to}
+              leaveActiveClass={s.slide_fade_leave_active}
+            >
+              {Content}
+            </Transition>
+          }
+        </RouterView>
       </main>
       <footer>
         <RouterView name='footer' />
