9736631bcb3c6a3dae76000f8ff12066836264d0
ClariS <1457715339@qq.com>
2022-08-01 19:17:19 +0800

    使用 replace 代替 push 避免后退

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -11,19 +11,19 @@ export const Welcome = defineComponent({
     const { direction, swiping } = useSwipe(main, { beforeStart: e => e.preventDefault() })
     const route = useRoute()
     const router = useRouter()
-    const pushMap: Record<string, string> = {
+    const replaceMap: Record<string, string> = {
       'Welcome1': '/welcome/2',
       'Welcome2': '/welcome/3',
       'Welcome3': '/welcome/4',
       'Welcome4': '/start',
     }    
-    const push = throttle(() => {
+    const replace = throttle(() => {
       const name = (route.name || 'Welcome1').toString()
-      router.push(pushMap[name])
+      router.replace(replaceMap[name])
     }, 500)
     watchEffect(() => {
       if (swiping.value && direction.value === 'left') {
-        push()
+        replace()
       }
     }) 
     return () => <div class={s.wrapper}>
