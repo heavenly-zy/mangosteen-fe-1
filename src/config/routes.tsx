bbe1659c4f2052cede256fd8876d5d546071ae83
ClariS <1457715339@qq.com>
2022-08-03 19:19:33 +0800

    feat: 创建 StartPage 和 Button

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -8,6 +8,7 @@ import { ThirdActions } from "../components/welcome/ThirdActions";
 import { Forth } from "../components/welcome/Forth";
 import { ForthActions } from "../components/welcome/ForthActions";
 import { Welcome } from "../views/Welcome";
+import { StartPage } from "../views/StartPage";
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', redirect: '/welcome' },
@@ -21,5 +22,6 @@ export const routes: RouteRecordRaw[] = [
       { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
       { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
     ]
-  }
+  },
+  { path:'/start', component: StartPage }
 ]
