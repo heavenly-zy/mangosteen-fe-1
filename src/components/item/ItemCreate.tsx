e38c2a98f0eea09b995f0487e49e43e575811f60
ClariS <1457715339@qq.com>
2022-08-16 16:05:14 +0800

    wip: /items/create 页面

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,15 +1,27 @@
 import { defineComponent, PropType } from 'vue';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
 import s from './ItemCreate.module.scss';
 
 export const ItemCreate = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
     return () => (
-      <div class={s.wrapper}>ItemCreate</div>
-    )
-  }
-})
+      <MainLayout>
+        {{
+          icon: () => <Icon name="left" class={s.navIcon} />,
+          title: () => '记一笔',
+          default: () => (
+            <>
+              <div>main</div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,7 +1,7 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss';
 
-export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' | 'charts' | 'notify' | 'export'
+export type IconName = 'add' | 'chart' | 'clock' | 'cloud' | 'mangosteen' | 'pig' | 'menu' | 'charts' | 'notify' | 'export' | 'left'
 
 export const Icon = defineComponent({
   props: {
