0d61fa3fee9b8ce230e115cba7bcd3bf325e8f43
ClariS <1457715339@qq.com>
2022-08-18 10:59:08 +0800

    feat: 完成 Tabs 组件

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -10,18 +10,18 @@ export const Tabs = defineComponent({
   },
   setup: (props, context) => {
     return () => {
-      const array = context.slots.default?.();
-      console.log(array);
-      if (!array) return () => null;
-      for (let i = 0; i < array.length; i++) {
-        if (array[i].type !== Tab) {
+      const tabs = context.slots.default?.();
+      console.log(tabs);
+      if (!tabs) return () => null;
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
           throw new Error('<Tabs> only accepts <Tab> as children');
         }
       }
       return (
         <div class={s.tabs}>
           <ol class={s.tabs_nav}>
-            {array.map((item) => (
+            {tabs.map((item) => (
               <li
                 class={item.props?.name === props.selected ? s.selected : ''}
                 onClick={() => context.emit('update:selected', item.props?.name)}
@@ -30,7 +30,9 @@ export const Tabs = defineComponent({
               </li>
             ))}
           </ol>
-          <div></div>
+          <div>
+            {tabs.find(item => item.props?.name === props.selected)}
+          </div>
         </div>
       );
     };
