30b6a0010ac12bbfad800a831ec60e3b923b39ca
ClariS <1457715339@qq.com>
2022-08-19 10:34:25 +0800

    wip: 制作数字按键

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -2,6 +2,7 @@ import { defineComponent, PropType, ref } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
 import { Tabs, Tab } from '../../shared/Tabs';
+import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
 
 export const ItemCreate = defineComponent({
@@ -22,6 +23,9 @@ export const ItemCreate = defineComponent({
               <Tab name="支出">icon 列表</Tab>
               <Tab name="收入">icon 列表2</Tab>
             </Tabs>
+            <div class={s.inputPad_wrapper}>
+              <InputPad />
+            </div>            
           </>,
         }}
       </MainLayout>
