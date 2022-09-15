884e04e1de0a44732cdf87d0ebb26f685d59af10
ClariS <1457715339@qq.com>
2022-09-15 08:22:55 +0000

    style: 固定 navbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,12 @@
 import { defineComponent } from 'vue';
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss';
 
 export const MainLayout = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <Navbar>
+      <div class={s.wrapper}>
+        <Navbar class={s.navbar}>
           {{
             icon: () => context.slots.icon?.(),
             default: () => context.slots.title?.(),
