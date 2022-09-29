b8e52a4c788a68decf647b061019ab5dd84bd516
ClariS <1457715339@qq.com>
2022-09-29 15:11:03 +0800

    feat: 完成登录页样式

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -1,5 +1,6 @@
 import { DatetimePicker, Popup } from 'vant';
 import { computed, defineComponent, PropType, ref, VNode } from 'vue';
+import { Button } from './Button';
 import { EmojiSelect } from './EmojiSelect';
 import s from './Form.module.scss';
 import { Time } from './time';
@@ -28,11 +29,12 @@ export const FormItem = defineComponent({
       type: [String, Number]
     },
     type: {
-      type: String as PropType<'text' | 'emojiSelect' | 'date'>,
+      type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode'>,
     },
     error: {
       type: String
-    }
+    },
+    placeholder: String
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
@@ -42,6 +44,7 @@ export const FormItem = defineComponent({
         case 'text':
           return <input
             value={props.modelValue}
+            placeholder={props.placeholder}
             onInput={(e: any) => context.emit('update:modelValue', e.target.value)}
             class={[s.formItem, s.input, s.error]} />
         case 'emojiSelect':
@@ -49,9 +52,18 @@ export const FormItem = defineComponent({
             modelValue={props.modelValue?.toString()}
             onUpdateModelValue={value => context.emit('update:modelValue', value)}
             class={[s.formItem, s.emojiList, s.error]} />
+        case 'validationCode':
+          return <>
+            <input class={[s.formItem, s.input, s.validationCodeInput]}
+              placeholder={props.placeholder} />
+            <Button class={[s.formItem, s.button, s.validationCodeButton]}>
+              发送验证码
+            </Button>
+          </>
         case 'date':
           return <>
             <input readonly={true} value={props.modelValue}
+              placeholder={props.placeholder}
               onClick={() => { refDateVisible.value = true }}
               class={[s.formItem, s.input]} />
             <Popup position='bottom' v-model:show={refDateVisible.value}>
@@ -76,11 +88,9 @@ export const FormItem = defineComponent({
           <div class={s.formItem_value}>
             {content.value}
           </div>
-          {props.error &&
-            <div class={s.formItem_errorHint}>
-              <span>{props.error}</span>
-            </div>
-          }
+          <div class={s.formItem_errorHint}>
+            <span>{props.error ?? '　'}</span>
+          </div>
         </label>
       </div>
     }
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,4 +1,9 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, reactive } from 'vue';
+import { MainLayout } from '../layouts/MainLayout';
+import { Button } from '../shared/Button';
+import { Form, FormItem } from '../shared/Form';
+import { Icon } from '../shared/Icon';
+import { validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 
 export const SignInPage = defineComponent({
@@ -8,8 +13,51 @@ export const SignInPage = defineComponent({
     }
   },
   setup: (props, context) => {
+    const formData = reactive({
+      email: '',
+      code: ''
+    })
+    const errors = reactive({
+      email: [],
+      code: []
+    })
+    const onSubmit = (e: Event) => {
+      e.preventDefault()
+      Object.assign(errors, {
+        email: [], code: []
+      })
+      Object.assign(errors, validate(formData, [
+        { key: 'email', type: 'required', message: '必填' },
+        { key: 'email', type: 'pattern', regex: /.+@.+/, message: '必须是邮箱地址' },
+        { key: 'code', type: 'required', message: '必填' },
+      ]))
+    }
     return () => (
-      <div class={s.wrapper}>登录</div>
+      <MainLayout>{
+        {
+          icon: () => <Icon name='left' />,
+          title: () => '登录',
+          default: () => (
+            <div class={s.wrapper}>
+              <div class={s.logo}>
+                <Icon class={s.icon} name="mangosteen" />
+                <h1 class={s.appName}>山竹记账</h1>
+              </div>
+              <Form onSubmit={onSubmit}>
+                <FormItem label="邮箱地址" type="text"
+                  placeholder='请输入邮箱，然后点击发送验证码'
+                  v-model={formData.email} error={errors.email?.[0]} />
+                <FormItem label="验证码" type="validationCode"
+                  placeholder='请输入六位数字'
+                  v-model={formData.code} error={errors.code?.[0]} />
+                <FormItem style={{ paddingTop: '96px' }}>
+                  <Button>登录</Button>
+                </FormItem>
+              </Form>
+            </div>
+          )
+        }
+      }</MainLayout>
     )
   }
 })
