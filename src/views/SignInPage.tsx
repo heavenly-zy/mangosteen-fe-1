6ba74fdcc4582c20c9a03cafb2ebf9a367d977c5
ClariS <1457715339@qq.com>
2023-04-12 18:18:55 +0800

    feat: 发送验证码成功之后触发倒计时

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType, reactive } from 'vue';
+import { defineComponent, PropType, reactive, ref } from 'vue';
 import { MainLayout } from '../layouts/MainLayout';
 import { Button } from '../shared/Button';
 import { Form, FormItem } from '../shared/Form';
@@ -22,6 +22,7 @@ export const SignInPage = defineComponent({
       email: [],
       code: []
     })
+    const refValidationCode = ref<any>()
     const onSubmit = (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
@@ -34,8 +35,12 @@ export const SignInPage = defineComponent({
       ]))
     }
     const onClickSendValidationCode = async () => {
-      // const response = await axios.post('/api/v1/validation_codes', { email: formData.email })
-      // console.log('response: ', response);
+      const response = await axios.post('/api/v1/validation_codes', { email: formData.email })
+        .catch(()=>{
+          // 失败
+        })
+      // 成功后开始倒计时
+      refValidationCode.value.startCount()
     }
     return () => (
       <MainLayout>{
@@ -52,7 +57,10 @@ export const SignInPage = defineComponent({
                 <FormItem label="邮箱地址" type="text"
                   placeholder='请输入邮箱，然后点击发送验证码'
                   v-model={formData.email} error={errors.email?.[0]} />
-                <FormItem label="验证码" type="validationCode"
+                <FormItem
+                  ref={refValidationCode}
+                  label="验证码" 
+                  type="validationCode"
                   placeholder='请输入六位数字'
                   countFrom={3}
                   v-model={formData.code} error={errors.code?.[0]}
