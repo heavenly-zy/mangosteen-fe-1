76bcfd5a234549f7c37f1865ae05b71f585c4af9
ClariS <1457715339@qq.com>
2023-04-13 22:21:04 +0800

    refactor: 优化路由守卫代码

--- src/main.ts
+++ src/main.ts
@@ -10,21 +10,27 @@ const router = createRouter({ history, routes })
 
 fetchMe()
 
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startsWith',
+  '/sign_in': 'startsWith',
+};
+
 router.beforeEach(async (to, from) => {
-  if (
-    to.path === '/' ||
-    to.path.startsWith('/welcome') ||
-    to.path.startsWith('/sign_in') ||
-    to.path === '/start'
-  ) {
-    return true;
-  } else {
-    const path = await mePromise!.then(
-      () => true,
-      () => '/sign_in?return_to=' + to.path
-    );
-    return path;
+  for (const key in whiteList) {
+    const value = whiteList[key];
+    if (value === 'exact' && to.path === key) {
+      return true;
+    }
+    if (value === 'startsWith' && to.path.startsWith(key)) {
+      return true;
+    }
   }
+  return mePromise!.then(
+    () => true,
+    () => '/sign_in?return_to=' + to.path
+  );
 });
 
 const app = createApp(App)
