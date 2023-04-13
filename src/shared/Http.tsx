21102aae80b72fbb503e49a62a95190578a57bac
ClariS <1457715339@qq.com>
2023-04-13 21:59:49 +0800

    feat: 对单个路由做 jwt 认证校验

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -65,6 +65,14 @@ export class Http {
 
 export const http = new Http('/api/v1');
 
+http.instance.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem('jwt');
+  if (jwt) {
+    config.headers!.Authorization = `Bearer ${jwt}`;
+  }
+  return config;
+});
+
 http.instance.interceptors.response.use(
   (response) => {
     console.log('response');
