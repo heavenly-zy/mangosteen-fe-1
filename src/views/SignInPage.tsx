66c266bf5a176af578ee167aa0e7dbead16f4a51
ClariS <1457715339@qq.com>
2023-04-13 11:51:15 +0800

    feat: 实现登录的 return to url

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -7,6 +7,7 @@ import { hasError, validate } from '../shared/validate';
 import s from './SignInPage.module.scss';
 import { http } from '../shared/Http';
 import { useBool } from '../hooks/useBool';
+import { useRoute, useRouter } from 'vue-router';
 
 export const SignInPage = defineComponent({
   props: {
@@ -25,6 +26,8 @@ export const SignInPage = defineComponent({
     })
     const refValidationCode = ref<any>()
     const { ref: refDisabled, on: disabled, off: enable } = useBool(false)
+    const route = useRoute()
+    const router = useRouter()
     const onSubmit = async (e: Event) => {
       e.preventDefault()
       Object.assign(errors, {
@@ -38,6 +41,9 @@ export const SignInPage = defineComponent({
       if (!hasError(errors)) {
         const response = await http.post<{ jwt: string }>('/session', formData)
         localStorage.setItem('jwt', response.data.jwt)
+        // router.push('/sign_in?return_to='+ encodeURIComponent(route.fullPath))
+        const returnTo = route.query.return_to?.toString()
+        router.push(returnTo || '/')
       }
     }
     const onError = (error: any) => {
