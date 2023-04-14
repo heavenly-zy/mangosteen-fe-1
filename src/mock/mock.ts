f70852ea554efce16f5bd0e4d37e2379bf16c444
ClariS <1457715339@qq.com>
2023-04-14 19:00:29 +0800

    feat: 创建 mock 模块

---
+++ src/mock/mock.ts
@@ -0,0 +1,15 @@
+import { faker } from '@faker-js/faker';
+import { AxiosRequestConfig } from 'axios';
+
+type Mock = (config: AxiosRequestConfig) => [number, any];
+
+faker.setLocale('zh_CN');
+
+export const mockSession: Mock = (config) => {
+  return [
+    200,
+    {
+      jwt: faker.random.word(),
+    },
+  ];
+};
--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,12 +1,10 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { mockSession } from '../mock/mock';
 
-type JSONValue =
-  | string
-  | number
-  | null
-  | boolean
-  | JSONValue[]
-  | { [key: string]: JSONValue };
+type GetConfig = Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>;
+type PostConfig = Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>;
+type PatchConfig = Omit<AxiosRequestConfig, 'url' | 'data'>;
+type DeleteConfig = Omit<AxiosRequestConfig, 'params'>;
 
 export class Http {
   instance: AxiosInstance;
@@ -20,7 +18,7 @@ export class Http {
   get<R = unknown>(
     url: string,
     query?: Record<string, string>,
-    config?: Omit<AxiosRequestConfig, 'params' | 'url' | 'method'>
+    config?: GetConfig
   ) {
     return this.instance.request<R>({
       ...config,
@@ -34,7 +32,7 @@ export class Http {
   post<R = unknown>(
     url: string,
     data?: Record<string, JSONValue>,
-    config?: Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+    config?: PostConfig
   ) {
     return this.instance.request<R>({ ...config, url, data, method: 'post' });
   }
@@ -43,7 +41,7 @@ export class Http {
   patch<R = unknown>(
     url: string,
     data?: Record<string, JSONValue>,
-    config?: Omit<AxiosRequestConfig, 'url' | 'data' | 'method'>
+    config?: PatchConfig
   ) {
     return this.instance.request<R>({ ...config, url, data, method: 'patch' });
   }
@@ -52,7 +50,7 @@ export class Http {
   delete<R = unknown>(
     url: string,
     query?: Record<string, string>,
-    config?: Omit<AxiosRequestConfig, 'url' | 'params' | 'method'>
+    config?: DeleteConfig
   ) {
     return this.instance.request<R>({
       ...config,
@@ -62,6 +60,33 @@ export class Http {
     });
   }
 }
+
+const mock = (response: AxiosResponse) => {
+  if (
+    location.hostname !== 'localhost' &&
+    location.hostname !== '127.0.0.1' &&
+    location.hostname !== '192.168.3.57'
+  ) {
+    return false;
+  }
+  switch (response.config?.params?._mock) {
+    // case 'tagIndex':
+    //   [response.status, response.data] = mockTagIndex(response.config);
+    //   return true;
+    // case 'itemCreate':
+    //   [response.status, response.data] = mockItemCreate(response.config);
+    //   return true;
+    // case 'itemIndex':
+    //   [response.status, response.data] = mockItemIndex(response.config);
+    //   return true;
+    // case 'tagCreate':
+    //   [response.status, response.data] = mockTagCreate(response.config);
+    case 'session':
+      [response.status, response.data] = mockSession(response.config);
+      return true;
+  }
+  return false;
+};
 
 export const http = new Http('/api/v1');
 
@@ -75,8 +100,20 @@ http.instance.interceptors.request.use((config) => {
 
 http.instance.interceptors.response.use(
   (response) => {
+    mock(response);
     return response;
   },
+  (error) => {
+    if (mock(error.response)) {
+      return error.response;
+    } else {
+      throw error;
+    }
+  }
+);
+
+http.instance.interceptors.response.use(
+  (response) => response,
   (error) => {
     if (error.response) {
       const axiosError = error as AxiosError;
