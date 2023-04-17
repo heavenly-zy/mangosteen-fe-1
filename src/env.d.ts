036e5d4cccdf3de5f29f39ef127b351a519ccbea
ClariS <1457715339@qq.com>
2023-04-17 14:53:50 +0800

    feat: 使用 prettier 来规范代码格式化

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
@@ -10,11 +10,11 @@ declare module '*.vue' {
 type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
 
 type Tag = {
-  id: number,
-  user_id: number,
-  name: string,
-  sign: string,
-  kind: 'expenses' | 'income'
+  id: number
+  user_id: number
+  name: string
+  sign: string
+  kind: "expenses" | "income"
 }
 
 type Item = {
@@ -23,14 +23,14 @@ type Item = {
   amount: number
   tags_id: number[]
   happen_at: string
-  kind: 'expenses' | 'income'
+  kind: "expenses" | "income"
 }
 
 type Resources<T = any> = {
   resources: T[]
   pager: {
-    page: number,
-    per_page: number,
+    page: number
+    per_page: number
     count: number
   }
 }
