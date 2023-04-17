0a95bc515b03d903d70b1f9655e6f4fa87132bf0
ClariS <1457715339@qq.com>
2023-04-17 14:30:29 +0800

    fix: vant 部分组件样式丢失

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { routes } from './config/routes'
 import { history } from './shared/history'
 import '@svgstore'
 import { fetchMe, mePromise } from './shared/me'
+/* --------------------------------
+Vant 中有个别组件是以函数的形式提供的，
+包括 Toast，Dialog，Notify 和 ImagePreview 组件。
+在使用函数组件时，unplugin-vue-components
+无法自动引入对应的样式，因此需要手动引入样式。
+------------------------------------- */
+import "vant/es/toast/style"
 
 const router = createRouter({ history, routes })
 
