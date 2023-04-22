9ccc4effae48eb908d10c4759c4eec839de2c447
ClariS <1457715339@qq.com>
2023-04-22 22:12:30 +0800

    feat: 退出登录 & 显示当前用户

--- src/env.d.ts
+++ src/env.d.ts
@@ -43,3 +43,8 @@ type Resource<T> = {
 type ResourceError = {
   errors: Record<string, string[]>
 }
+
+type User = {
+  id: number;
+  email: string;
+}
--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,7 +1,9 @@
-import { defineComponent, PropType, ref } from 'vue';
-import { RouterLink } from 'vue-router';
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
 import { Icon } from './Icon';
 import s from './Overlay.module.scss';
+import { mePromise } from './me';
+import { Dialog } from 'vant';
 
 export const Overlay = defineComponent({
   props: {
@@ -13,13 +15,35 @@ export const Overlay = defineComponent({
     const close = () => {
       props.onClose?.()
     }
-    const onClickSignIn = () => { }
+    const route = useRoute()
+    const me = ref<User>()
+    onMounted(async () => {
+      const response = await mePromise
+      me.value = response?.data.resource
+    })
+    const onSignOut = async () => {
+      await Dialog.confirm({
+        title: "确认",
+        message: "你真的要退出登录吗？"
+      })
+      localStorage.removeItem("jwt")
+      location.reload()
+    }
     return () => <>
       <div class={s.mask} onClick={close}></div>
       <div class={s.overlay}>
-        <section class={s.currentUser} onClick={onClickSignIn}>
-          <h2>未登录用户</h2>
-          <p>点击这里登录</p>
+        <section class={s.currentUser}>
+          {me.value ? (
+            <div>
+              <h2 class={s.email}>{me.value.email}</h2>
+              <p onClick={onSignOut}>点击这里退出登录</p>
+            </div>
+          ) : (
+            <RouterLink to={`/sign_in?return_to=${route.fullPath}`}>
+              <h2>未登录用户</h2>
+              <p>点击这里登录</p>
+            </RouterLink>
+          )}
         </section>
         <nav>
           <ul class={s.action_list}>
