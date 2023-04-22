91e9433e19793ca3c19657b37b0f6f9199b699c6
ClariS <1457715339@qq.com>
2023-04-23 00:09:42 +0800

    feat: 初步转换折线图数据

--- src/mock/mock.ts
+++ src/mock/mock.ts
@@ -136,3 +136,17 @@ export const mockItemIndexBalance: Mock = () => {
     balance: 0
   }]
 }
+
+export const mockItemSummary: Mock = () => {
+  return [
+    200,
+    {
+      groups: [
+        { happen_at: "2018-06-18T00:00:00.000+0800", amount: 100 },
+        { happen_at: "2018-06-22T00:00:00.000+0800", amount: 300 },
+        { happen_at: "2018-06-29T00:00:00.000+0800", amount: 200 }
+      ],
+      summary: 600
+    }
+  ]
+}
