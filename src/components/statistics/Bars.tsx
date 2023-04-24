ccd4345bde2e98ec2db692a93db3144bb5031ef9
ClariS <1457715339@qq.com>
2023-04-24 11:30:53 +0800

    feat: 完成 bar 图表

--- src/components/statistics/Bars.tsx
+++ src/components/statistics/Bars.tsx
@@ -1,46 +1,36 @@
-import { computed, defineComponent, PropType, reactive } from "vue"
+import { defineComponent, PropType } from "vue"
 import s from "./Bars.module.scss"
+import { Money } from "../../shared/Money";
 
 export const Bars = defineComponent({
   props: {
-    name: {
-      type: String as PropType<string>
+    data: {
+      type: Array as PropType<{ tag: Tag; amount: number; percent: number }[]>
     }
   },
   setup: (props, context) => {
-    const data3 = reactive([
-      { tag: { id: 1, name: "房租", sign: "x" }, amount: 3000 },
-      { tag: { id: 2, name: "吃饭", sign: "x" }, amount: 1000 },
-      { tag: { id: 3, name: "娱乐", sign: "x" }, amount: 900 }
-    ])
-    const betterData3 = computed(() => {
-      const total = data3.reduce((sum, item) => sum + item.amount, 0)
-      return data3.map((item) => ({
-        ...item,
-        percent: Math.round((item.amount / total) * 100) + "%"
-      }))
-    })
     return () => (
       <div class={s.wrapper}>
-        {betterData3.value.map(({ tag, amount, percent }) => {
-          return (
-            <div class={s.topItem}>
-              <div class={s.sign}>{tag.sign}</div>
-              <div class={s.bar_wrapper}>
-                <div class={s.bar_text}>
-                  <span>
-                    {" "}
-                    {tag.name} - {percent}{" "}
-                  </span>
-                  <span> ￥{amount} </span>
-                </div>
-                <div class={s.bar}>
-                  <div class={s.bar_inner}></div>
+        {props.data && props.data.length > 0 ? (
+          props.data.map(({ tag, amount, percent }) => {
+            return (
+              <div class={s.topItem}>
+                <div class={s.sign}>{tag.sign}</div>
+                <div class={s.bar_wrapper}>
+                  <div class={s.bar_text}>
+                    <span>{tag.name} - {percent}%</span>
+                    <span>￥<Money value={amount} /></span>
+                  </div>
+                  <div class={s.bar}>
+                    <div class={s.bar_inner} style={{ width: `${percent}%` }}></div>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })
+        ) : (
+          <div>暂无数据</div>
+        )}
       </div>
     )
   }
--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -71,6 +71,16 @@ export const Charts = defineComponent({
       })
       data2.value = response.data.groups
     })
+
+    // data3 
+
+    const betterData3 = computed<{ tag: Tag; amount: number; percent: number }[]>(() => {
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+      return data2.value.map((item) => ({
+        ...item,
+        percent: Math.round((item.amount / total) * 100)
+      }))
+    })
     return () => (
       <div class={s.wrapper}>
         <FormItem
@@ -84,7 +94,7 @@ export const Charts = defineComponent({
         />
         <LineChart data={betterData1.value} />
         <PieChart data={betterData2.value} />
-        <Bars />
+        <Bars data={betterData3.value} />
       </div>
     )
   }
