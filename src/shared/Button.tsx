3d80beea704874462ce5183afedc9fee59ae39b1
ClariS <1457715339@qq.com>
2023-05-08 11:03:54 +0800

    chore: 升级到 Vite 3.0.0 & 修复升级后的打包错误

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -35,8 +35,8 @@ export const Button = defineComponent({
         return props.disabled
       }
     })
-    const onClick = () => {
-      props.onClick?.()
+    const onClick = (e: MouseEvent) => {
+      props.onClick?.(e)
       selfDisabled.value = true
       setTimeout(() => {
         selfDisabled.value = false
