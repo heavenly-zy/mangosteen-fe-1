f7f3f21d5a0159701af49f907be89731ec022323
ClariS <1457715339@qq.com>
2023-05-15 11:10:48 +0800

    fix: 修复错误地请求了下一页 items 的 bug

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -100,7 +100,7 @@ export const ItemSummary = defineComponent({
           ))}
         </ol>
         <div class={s.more}>
-          {itemStore.hasMore ? <Button onClick={() => itemStore.fetchItems(props.startDate!, props.endDate!)}>加载更多</Button> : <span>没有更多</span>}
+          {itemStore.hasMore ? <Button onClick={() => itemStore.fetchNextPage(props.startDate!, props.endDate!)}>加载更多</Button> : <span>没有更多</span>}
         </div>
       </>
     ))
