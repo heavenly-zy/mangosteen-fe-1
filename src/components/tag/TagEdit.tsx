15d96b817d5d09679f79431a697e845b35610929
ClariS <1457715339@qq.com>
2023-05-15 15:01:51 +0800

    fix: 只保留一种删除标签的方式

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -42,11 +42,8 @@ export const TagEdit = defineComponent({
             <>
               <TagForm id={tagId} />
               <div class={s.actions}>
-                <Button level="danger" class={s.removeTags} onClick={() => onDelete()}>
-                  删除标签
-                </Button>
                 <Button level="danger" class={s.removeTagsAndItems} onClick={() => onDelete({ withItems: true })}>
-                  删除标签和记账
+                  删除标签（对应记账也会被删除）
                 </Button>
               </div>
             </>
